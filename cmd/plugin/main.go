dccf07091fe869fc277c394dd66397586642e7db
Joseph Burnett <josephburnett79@gmail.com>
2019-08-17 22:23:20 +0200

    Create and delete autoscalers.

--- cmd/plugin/main.go
+++ cmd/plugin/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"sync"
+	"fmt"
+	"log"
 
 	knativeplugin "knative.dev/serving/pkg/plugin"
 	"github.com/josephburnett/sk-plugin/pkg/skplug"
@@ -21,11 +23,30 @@ type pluginServer struct {
 }
 
 func newPluginServer() *pluginServer {
-	return &pluginServer{}
+	return &pluginServer{
+		autoscalers: make(map[string]*knativeplugin.Autoscaler),
+	}
 }
 
 func (p *pluginServer) Event(part string, time int64, typ proto.EventType, object skplug.Object) error {
-	return nil
+	switch o := object.(type) {
+	case *skplug.Autoscaler:
+		switch typ {
+		case proto.EventType_CREATE:
+			return p.createAutoscaler(part, o)
+		case proto.EventType_UPDATE:
+			return fmt.Errorf("update autoscaler event not supported")
+		case proto.EventType_DELETE:
+			return p.deleteAutoscaler(part)
+		default:
+			return fmt.Errorf("unhandled event type: %v for object of type: %T", typ, object)
+		}
+	case *skplug.Pod:
+		// TODO: keep track of Pods.
+		return nil
+	default:
+		return fmt.Errorf("unhandled object type: %T", object)
+	}
 }
 
 func (p *pluginServer) Stat(part string, stat []*proto.Stat) error {
@@ -34,6 +55,32 @@ func (p *pluginServer) Stat(part string, stat []*proto.Stat) error {
 
 func (p *pluginServer) Scale(part string, time int64) (rec int32, err error) {
 	return 0, nil
+}
+
+func (p *pluginServer) createAutoscaler(part string, a *skplug.Autoscaler) error {
+	p.mux.Lock()
+	defer p.mux.Unlock()
+	if _, ok := p.autoscalers[part]; ok {
+		return fmt.Errorf("duplicate create autoscaler event in partition %v", part)
+	}
+	autoscaler, err := knativeplugin.NewAutoscaler(a.Yaml)
+	if err != nil {
+		return err
+	}
+	p.autoscalers[part] = autoscaler
+	log.Println("created autoscaler", part)
+	return nil
+}
+
+func (p *pluginServer) deleteAutoscaler(part string) error {
+	p.mux.Lock()
+	defer p.mux.Unlock()
+	if _, ok := p.autoscalers[part]; !ok {
+		return fmt.Errorf("delete autoscaler event for non-existant partition %v", part)
+	}
+	delete(p.autoscalers, part)
+	log.Println("deleted autoscaler", part)
+	return nil
 }
 
 func main() {
--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	
 	"github.com/josephburnett/sk-plugin/pkg/skplug/proto"
+	"knative.dev/serving/pkg/resources"
 	"knative.dev/serving/pkg/autoscaler"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -14,9 +15,22 @@ type Autoscaler struct {
 	collector *autoscaler.MetricCollector
 }
 
-func NewAutoscaler() *Autoscaler {
-	// TODO: construct Knative Autoscaler and MetricCollector.
-	return &Autoscaler{}
+func NewAutoscaler(yaml string) (*Autoscaler, error) {
+	// TODO: construct metric collector
+	a, err := autoscaler.New(
+		"default",
+		"autoscaler",
+		&fakeMetricClient{}, // TODO: provide metric collector
+		&fakeReadyPodCounter{}, // TODO: provide count of pods
+		autoscaler.DeciderSpec{}, // TODO: parse PodAutoscaler
+		&fakeStatsReporter{},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &Autoscaler{
+		autoscaler: a,
+	}, nil
 }
 
 func (a *Autoscaler) Stat(stats []*proto.Stat) error {
@@ -41,5 +55,67 @@ func (a *Autoscaler) Stat(stats []*proto.Stat) error {
 func (a *Autoscaler) Scale(now int64) (int32, error) {
 	desiredPodCount, _, _ := a.autoscaler.Scale(context.TODO(), time.Unix(0, now))
 	return desiredPodCount, nil
+}
+
+var _ autoscaler.MetricClient = &fakeMetricClient{}
+
+type fakeMetricClient struct {
+	collector *autoscaler.MetricCollector
+}
+
+func (f *fakeMetricClient) StableAndPanicConcurrency(key types.NamespacedName, now time.Time) (float64, float64, error) {
+	// TODO: report concurrency metrics collected by collector.
+	return 1.0, 1.0, nil
+}
+
+func (f *fakeMetricClient) StableAndPanicOPS(key types.NamespacedName, now time.Time) (float64, float64, error) {
+	return 1.0, 1.0, nil
+}
+
+var _ resources.ReadyPodCounter = &fakeReadyPodCounter{}
+
+type fakeReadyPodCounter struct {
+	
+}
+
+func (f *fakeReadyPodCounter) ReadyCount() (int, error) {
+	// TODO: keep track of pods.
+	return 0, nil
+}
+
+var _ autoscaler.StatsReporter = &fakeStatsReporter{}
+
+type fakeStatsReporter struct {}
+
+func (f *fakeStatsReporter) ReportDesiredPodCount(v int64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportRequestedPodCount(v int64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportActualPodCount(v int64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportStableRequestConcurrency(v float64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportPanicRequestConcurrency(v float64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportTargetRequestConcurrency(v float64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportExcessBurstCapacity(v float64) error {
+	return nil
+}
+
+func (f *fakeStatsReporter) ReportPanic(v int64) error {
+	return nil
 }
 
